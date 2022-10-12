4ef4de061d97206db917cf2379ac47384e65c6f2
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-12 14:59:47 +0530

    encrypt private key, use functions with contract

---
+++ ethers-simple-storage/encryptKey.js
@@ -0,0 +1,19 @@
+const ethers = require("ethers");
+const fs = require("fs");
+require("dotenv").config();
+
+async function main() {
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
+  const encryptedJsonkey = await wallet.encrypt(
+    process.env.PRIVATE_KEY_PASSWORD,
+    process.env.PRIVATE_KEY
+  );
+  fs.writeFileSync('./.encryptedKey.json', encryptedJsonkey)
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((err) => {
+    console.log(err);
+    process.exit(1);
+  });
