cde52d5401faba0c2aae1258ecf495b8909762be
aadarsh805 <aadarshthakur80501@gmail.com>
2022-10-12 16:12:42 +0530

    deployed to alchemy goerli testnet

--- ethers-simple-storage/encryptKey.js
+++ ethers-simple-storage/encryptKey.js
@@ -1,19 +1,24 @@
-const ethers = require("ethers");
-const fs = require("fs");
-require("dotenv").config();
+const ethers = require("ethers")
+const fs = require("fs")
+require("dotenv").config()
+
+// typescript
+// import {ethers} from 'ethers'
+// import * as fs from "fs"
+// import "dotenv/config"
 
 async function main() {
-  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY);
-  const encryptedJsonkey = await wallet.encrypt(
-    process.env.PRIVATE_KEY_PASSWORD,
-    process.env.PRIVATE_KEY
-  );
-  fs.writeFileSync('./.encryptedKey.json', encryptedJsonkey)
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+    const encryptedJsonkey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD,
+        process.env.PRIVATE_KEY
+    )
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonkey)
 }
 
 main()
-  .then(() => process.exit(0))
-  .catch((err) => {
-    console.log(err);
-    process.exit(1);
-  });
+    .then(() => process.exit(0))
+    .catch((err) => {
+        console.log(err)
+        process.exit(1)
+    })
